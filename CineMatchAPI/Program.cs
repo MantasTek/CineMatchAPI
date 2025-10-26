ad4cdae4616c094eee9162268268ae798c80f5ab
mantas petrovas <mantas10@gmail.com>
2025-10-26 16:07:05 +0100

    corrected url

--- CineMatchAPI/Program.cs
+++ CineMatchAPI/Program.cs
@@ -81,7 +81,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:8081")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
