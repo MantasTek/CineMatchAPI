6d38ea0045d10480ade43618f4d4d10899762cbb
mantas petrovas <mantas10@gmail.com>
2025-11-04 20:21:54 +0100

    something

--- CineMatchAPI/Migrations/20251104175641_MakePreferencesNullable.Designer.cs
+++ CineMatchAPI/Migrations/20251104175641_MakePreferencesNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CineMatchAPI.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CineMatchAPI.Migrations
 {
     [DbContext(typeof(CineMatchDbContext))]
-    partial class CineMatchDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251104175641_MakePreferencesNullable")]
+    partial class MakePreferencesNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
